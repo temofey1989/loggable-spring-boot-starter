6196f83cb54f0a00c1394555516a8cc41a945f66
Artyom Gornostayev <temofey1989@gmail.com>
2021-12-13 11:02:49 +0100

    Log format property rename.

--- src/main/kotlin/io/justdevit/spring/logging/Constants.kt
+++ src/main/kotlin/io/justdevit/spring/logging/Constants.kt
@@ -1,6 +1,6 @@
 package io.justdevit.spring.logging
 
-const val ACTION_LOG_FORMAT_PARAMETER = "logging.action-log-format"
+const val ACTION_LOG_FORMAT_PARAMETER = "logging.actions.log-format"
 const val CONSOLE_LOG_FORMAT = "console"
 const val LOGSTASH_JSON_LOG_FORMAT = "logstash-json"
 
